f4fc57b6f2a81e9fd60df68b572da030139c1d9b
angenue <angelica1nuesi@gmail.com>
2024-03-30 13:38:35 -0400

    created player class to store coins

---
+++ src/main/java/org/example/Player.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Player {
+    private int amountOfCash = 50;
+
+    public Player() {
+    }
+
+    public int getAmountOfCash() {
+        return amountOfCash;
+    }
+
+    public void setAmountOfCash(int amountOfCash) {
+        this.amountOfCash = amountOfCash;
+    }
+}
