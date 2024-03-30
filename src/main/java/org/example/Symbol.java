3460c96b43e1c9e35810d795abf596793760509d
angenue <angelica1nuesi@gmail.com>
2024-03-30 13:28:00 -0400

    add utf symbols to enum

---
+++ src/main/java/org/example/Symbol.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public enum Symbol {
+    WATERMELON("\uD83C\uDF49"),
+    GRAPE("\uD83C\uDF47"),
+    ORANGE("\uD83C\uDF4A"),
+    BANANA("\uD83C\uDF4C"),
+    CHERRY("\uD83C\uDF52"),
+    SEVEN("7️⃣");
+    private final String symbol;
+
+    Symbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+}
